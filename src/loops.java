062922262d55d079430b0fff27804891aaa5bd64
bhanu527 <regmibhanubhakta67@gmail.com>
2023-08-13 21:58:35 +0545

    java loop

---
+++ src/loops.java
@@ -0,0 +1,11 @@
+public class loops {
+    //learning about loops
+    public static void main(String[] args) {
+        //Q. print number form 1 to 5
+        //for loop is used when the num of iteration is known.
+        for( int a=0;a<=5;a++){
+            System.out.println(a);
+        }
+
+    }
+}
