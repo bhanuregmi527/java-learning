13aa106b0dc24346986e41d1c9f51ceeaafe5f87
bhanu527 <regmibhanubhakta67@gmail.com>
2023-08-18 07:50:33 +0545

    .

--- src/Binary_Search/searchRange.java
+++ src/Binary_Search/searchRange.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class searchRange {
+ //Searching for start and end index of array
     public static void main(String[] args) {
         int[]nums={5,7,7,7,7,8,8,10};
         int target=7;
